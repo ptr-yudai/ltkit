7243e293ea0bb320fe73ea91e632b425e1078c81
ptr-yudai <ptr.yudai@gmail.com>
2016-04-26 17:31:29 +0900

    fiexed some code

--- src/ltkit/mod_viewer.py
+++ src/ltkit/mod_viewer.py
@@ -41,43 +41,65 @@ class MessageViewer:
         
         Attributes:
             frame ... a window frame which has a message on
-            panel ... a panel to put a message on
+            message_bitmap ... a masked bitmap image of the message
+            timer ... a timer to move the frame
+            
         """
         def __init__(self, message_struct):
             """initializes MessageWindow"""
-            # new Screenshot class
-            self.screenshot = mod_screenshot.Screenshot()
+            # passes a filter
+            self.message_filter(message_struct)
             # new visible frame (message)
             self.frame = wx.Frame(None,
                                   wx.ID_ANY,
                                   message_struct["message"],
-                                  style = wx.FRAME_SHAPED | wx.NO_BORDER | wx.CLOSE_BOX )
+                                  style = wx.FRAME_SHAPED
+                                  | wx.NO_BORDER
+                                  | wx.CLOSE_BOX
+                                  | wx.STAY_ON_TOP)
             # creates message bitmap
             self.message_bitmap = self.create_bitmap_message(message_struct)
-            self.OnCreate("")
+            self.OnCreate(None)
             # calculates window size
-            self.frame.SetPosition(message_struct["position"])
+            self.frame_pos = [message_struct["position"][0], message_struct["position"][1]]
+            self.frame.SetPosition(self.frame_pos)
             self.frame.SetClientSize(self.message_bitmap.GetSize())
             # sets shape
             self.create_shape()
             self.frame.Bind(wx.EVT_PAINT, self.OnPaint)
             self.frame.Bind(wx.EVT_WINDOW_CREATE, self.OnCreate)
+            # sets timer
+            self.timer = wx.Timer(self.frame)
+            self.frame.Bind(wx.EVT_TIMER, self.OnTimer)
+            self.timer.Start(message_struct['speed'])
             # shows this frame
             self.frame.Show(True)
             return None
 
-        def OnPaint(self, obj):
+        def OnTimer(self, event):
+            """on timer"""
+            # moves the frame
+            self.frame_pos[0] -= 1
+            self.frame.SetPosition((self.frame_pos[0], self.frame_pos[1]))
+            if self.frame_pos[0] <= -self.frame_size[0]:
+                # kill the frame when it goes out of the screen
+                self.timer.Stop()
+                self.frame.Destroy()
+            return None
+
+        def OnPaint(self, event):
             """on paint"""
             dc = wx.PaintDC(self.frame)
             dc.DrawBitmap(self.message_bitmap, 0, 0, True)
             return None
 
-        def OnCreate(self, obj):
+        def OnCreate(self, event):
             """on create"""
             self.frame.SetShape(wx.RegionFromBitmap(self.message_bitmap))
             return None
 
         def create_shape(self):
+            """creates a shaped window"""
             dc = wx.ClientDC(self.frame)
             dc.DrawBitmap(self.message_bitmap, 0, 0, True)
             self.frame.SetShape(wx.RegionFromBitmap(self.message_bitmap))
@@ -94,16 +116,16 @@ class MessageViewer:
             # calculates message width and height
             dc = wx.MemoryDC()
             dc.SetFont(font)
-            size = dc.GetTextExtent(message_struct['message'])
+            self.frame_size = dc.GetTextExtent(message_struct['message'])
             # new bitmap
-            bitmap = wx.EmptyBitmap(size[0], size[1])
+            bitmap = wx.EmptyBitmap(self.frame_size[0], self.frame_size[1])
             # draws text for shaping
             dc.SelectObject(bitmap)
             dc.Clear()
             dc.BeginDrawing()
             dc.SetPen(wx.Pen(mask_color, 0))
             dc.SetBrush(wx.Brush(mask_color, wx.SOLID))
-            dc.DrawRectangle(0, 0, size[0], size[1])
+            dc.DrawRectangle(0, 0, self.frame_size[0], self.frame_size[1])
             dc.SetTextForeground(message_struct['color'])
             #dc.SetAntialiasMode(wx.ANTIALIAS_NONE)
             dc.DrawText(message_struct['message'], 0, 0)
@@ -118,10 +140,24 @@ class MessageViewer:
 
         def create_mask_color(self, mask_color):
             """creates a mask color by text color"""
-            print mask_color
             mask_color = list(mask_color)
             if mask_color[0] == 0xFF:
+                # if the first element of the color is 255
                 mask_color[0] -= 1
             else:
+                # if the first element of the color is not 255
                 mask_color[0] += 1
             return tuple(mask_color)
+
+        def message_filter(self, message_struct):
+            if message_struct.get('message', None) == None:
+                return False
+            if message_struct.get('position', None) == None:
+                return False
+            if message_struct.get('size', None) == None:
+                message_struct['size'] = 32
+            if message_struct.get('color', None) == None:
+                message_struct['color'] = (240, 240, 240)
+            if message_struct.get('speed', None) == None:
+                message_struct['speed'] = 5
+            return True
--- src/modtest_message.py
+++ src/modtest_message.py
@@ -7,12 +7,20 @@ def main():
     message_viewer = mod_viewer.MessageViewer(ltkit)
 
     # debug data
-    message = {"message":"PythonでLTのやつ",
-               "position":(0, 0),
+    message = {"message":"This is a sample message...!",
+               "position":(wx.DisplaySize()[0], 0),
                "size":48,
-               "color":(255, 0, 255)}
+               "color":(255, 0, 255),
+               "speed":5}
     message_viewer.display_text(message)
-
+    """
+    message = {"message":"Are you sure?",
+               "position":(wx.DisplaySize()[0], 0),
+               "size":32,
+               "color":(255, 0, 0),
+               "speed":50}
+    message_viewer.display_text(message)
+    """
     ltkit.MainLoop()
     return
 
