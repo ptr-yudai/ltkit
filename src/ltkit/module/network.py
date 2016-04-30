e0ce5955b5cc355f52138e9f8235bd27383c8ce7
ptr-yudai <ptr.yudai@gmail.com>
2016-04-30 09:59:55 +0900

    fixed : panel and module

---
+++ src/ltkit/module/network.py
@@ -0,0 +1,15 @@
+import socket
+
+class Network:
+    """ Networking System.
+    This class is used for any kind of connection
+    between client and server.
+    """
+    def __init__(self):
+        return
+
+    def create_socket(self):
+        return
+    
+    def destroy_socket(self):
+        return
