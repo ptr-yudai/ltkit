b2206ffbbe180d84ad377da119ec945c4b5d9523
ptr-yudai <ptr.yudai@gmail.com>
2016-05-01 20:30:58 +0900

    fixed : class structure

---
+++ src/ltkit/panel/server/questionary.py
@@ -0,0 +1,69 @@
+import wx
+
+class Panel(wx.Panel):
+    """ Creates a panel of the questionary
+    This class is a panel which has all functions for the questionaries.
+    """
+    def __init__(self, parent):
+        """ Initialize Panel class """
+        # new panel
+        wx.Panel.__init__(self,
+                          parent)
+        # new font
+        DEFAULT_FONT = wx.Font(12,
+                               wx.FONTFAMILY_DEFAULT,
+                               wx.FONTSTYLE_NORMAL,
+                               wx.FONTWEIGHT_NORMAL)
+        # Create sizer
+        self.layout_main = wx.BoxSizer(wx.VERTICAL)
+        self.layout = []
+        for i in range(3):
+            self.layout.append(wx.BoxSizer(wx.HORIZONTAL))
+        # text = "No questionary"
+        label = wx.StaticText(self,
+                              id = wx.ID_ANY,
+                              label = u"Question : ")
+        label.SetFont(DEFAULT_FONT)
+        self.layout[0].Add(label,
+                           flag = wx.EXPAND | wx.ALIGN_LEFT | wx.ALL,
+                           border = 8)
+        # Create a text control to enter the question
+        self.text_question = wx.TextCtrl(self,
+                                         id = wx.ID_ANY,
+                                         size = (320, 32))
+        self.text_question.SetFont(DEFAULT_FONT)
+        self.text_question.SetToolTipString(u"Qeustion")
+        self.layout[0].Add(self.text_question,
+                           flag = wx.EXPAND | wx.ALIGN_LEFT | wx.ALL,
+                           border = 0)
+        # Create a list control
+        self.list_question = wx.ListBox(self,
+                                        wx.ID_ANY,
+                                        choices = (),
+                                        style = wx.LB_SINGLE
+                                        | wx.LB_HSCROLL
+                                        | wx.LB_NEEDED_SB)
+        self.list_question.SetFont(DEFAULT_FONT)
+        self.layout[1].Add(self.list_question,
+                           proportion = 1,
+                           flag = wx.EXPAND | wx.ALIGN_LEFT | wx.ALL,
+                           border = 8)
+        # Create a button
+        self.button_add = wx.Button(self,
+                                    id = wx.ID_ANY,
+                                    label = u"Add a choice",
+                                    size = (128, 32))
+        self.button_add.SetFont(DEFAULT_FONT)
+#        self.button_add.Bind(wx.EVT_BUTTON, self.inet.create_socket)
+        self.layout[2].Add(self.button_add,
+                           flag = wx.EXPAND | wx.ALIGN_RIGHT | wx.ALL,
+                           border = 8)
+        # Set sizer
+        self.layout_main.Add(self.layout[0], flag = wx.ALIGN_TOP)
+        self.layout_main.Add(self.layout[1],
+                             proportion = 1,
+                             flag = wx.EXPAND | wx.ALIGN_CENTER)
+        self.layout_main.Add(self.layout[2], flag = wx.ALIGN_BOTTOM | wx.RIGHT) 
+        self.SetSizer(self.layout_main)
+        return
+        
